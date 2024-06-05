fb6f3bcfd6eb99f8edc29dd0536d56c7613dd296
Omico <me@omico.icu>
2024-06-04 19:52:11 -0700

    resources: Add achievement files

--- wwm/core/resources/generator/src/main/kotlin/dev/omico/wwm/resources/generator/WwmResourceGenerator.kt
+++ wwm/core/resources/generator/src/main/kotlin/dev/omico/wwm/resources/generator/WwmResourceGenerator.kt
@@ -7,6 +7,9 @@ package dev.omico.wwm.resources.generator
 
 import dev.omico.wwm.resources.generator.internal.game.WwData
 import dev.omico.wwm.resources.generator.internal.writeTo
+import dev.omico.wwm.resources.model.game.WwAchievement
+import dev.omico.wwm.resources.model.game.WwAchievementCategory
+import dev.omico.wwm.resources.model.game.WwAchievementGroup
 import dev.omico.wwm.resources.model.game.WwText
 import dev.omico.wwm.resources.runtime.WwmResourcesPaths
 import java.nio.file.Path
@@ -16,8 +19,10 @@ import kotlin.io.path.createParentDirectories
 fun main(arguments: Array<String>) {
     val gameDataDirectory = Path(arguments[0])
     val outputDirectory = Path(arguments[1])
-    val wwData = WwData(gameDataDirectory)
-    wwData.generateMultiTextFiles(outputDirectory)
+    WwData(gameDataDirectory).run {
+        generateMultiTextFiles(outputDirectory)
+        saveAchievementFiles(outputDirectory)
+    }
 }
 
 private fun WwData.generateMultiTextFiles(outputDirectory: Path) {
@@ -34,11 +39,24 @@ private fun WwData.generateMultiTextFiles(outputDirectory: Path) {
         }
     }
     multiTextMap.forEach { (locale, multiText) ->
-        val outputMultiTextFile = outputDirectory.resolve(WwmResourcesPaths.multiTextPath(locale))
-            .createParentDirectories()
         multiText
-            .filter { it.id in textIds }
+            .filter { text -> text.id in textIds }
             .sortedBy(WwText::id)
-            .writeTo(outputMultiTextFile)
+            .saveToFile(outputDirectory, WwmResourcesPaths.multiTextPath(locale))
     }
 }
+
+private fun WwData.saveAchievementFiles(outputDirectory: Path) {
+    achievements
+        .sortedBy(WwAchievement::id)
+        .saveToFile(outputDirectory, WwmResourcesPaths.AchievementPath)
+    achievementCategories
+        .sortedBy(WwAchievementCategory::id)
+        .saveToFile(outputDirectory, WwmResourcesPaths.AchievementCategoryPath)
+    achievementGroups
+        .sortedWith(compareBy(WwAchievementGroup::id, WwAchievementGroup::sort))
+        .saveToFile(outputDirectory, WwmResourcesPaths.AchievementGroupPath)
+}
+
+private inline fun <reified T> T.saveToFile(outputDirectory: Path, path: String) =
+    writeTo(outputDirectory.resolve(path).createParentDirectories())
--- wwm/core/resources/runtime/src/commonMain/kotlin/dev/omico/wwm/resources/runtime/WwmResourcesPaths.kt
+++ wwm/core/resources/runtime/src/commonMain/kotlin/dev/omico/wwm/resources/runtime/WwmResourcesPaths.kt
@@ -5,6 +5,10 @@ package dev.omico.wwm.resources.runtime
 
 import dev.omico.wwm.resources.model.game.WwLocale
 
+@Suppress("ConstPropertyName")
 object WwmResourcesPaths {
+    const val AchievementPath: String = "files/Achievement.json"
+    const val AchievementCategoryPath: String = "files/AchievementCategory.json"
+    const val AchievementGroupPath: String = "files/AchievementGroup.json"
     fun multiTextPath(locale: WwLocale): String = "files/MultiText.$locale.json"
 }
